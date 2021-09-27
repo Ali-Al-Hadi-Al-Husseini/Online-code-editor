f4a076698bcfdc1f988f8e63448869241aa20c9e
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-09-27 15:29:33 +0300

    Updates

---
+++ precache-manifest.71dbf54ad999eb2b6b78ae3b11fb5cbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6dbd43873e5ec256bf9da1f017d01ef",
+    "url": "/online-code-editor/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/online-code-editor/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "aedf52ebb032c64f5783",
+    "url": "/online-code-editor/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/online-code-editor/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-code-editor/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedf52ebb032c64f5783",
+    "url": "/online-code-editor/static/js/main.f7bb02a2.chunk.js"
+  },
+  {
+    "revision": "6c037b93c95f6c4d2cf5",
+    "url": "/online-code-editor/static/js/runtime-main.1a6b4da7.js"
+  }
+]);
